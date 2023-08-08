f95d1d61eb13f05a1cb6f5a8e6815ae49bf515af
PrathameshLad <117907892+LadPrathamesh@users.noreply.github.com>
2023-08-08 20:16:22 +0530

    Initial commit

---
+++ TemplatePr2h/src/main/java/MavenTemplatePr2h/TemplatePr2h/App.java
@@ -0,0 +1,13 @@
+package MavenTemplatePr2h.TemplatePr2h;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
