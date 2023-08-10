9fc6651d4a674e882767c29f7ba87ee86b97f309
PrathameshLad <117907892+LadPrathamesh@users.noreply.github.com>
2023-08-10 22:45:25 +0530

    TesterLogin
    TesterLogin

--- TemplatePr2h/src/main/java/pageLayer/LoginPage.java
+++ TemplatePr2h/src/main/java/pageLayer/LoginPage.java
@@ -39,5 +39,14 @@ public class LoginPage extends TestBase{
 		loginButton.click();
 		}
 	//=====================================================================
+	public LoginPage login;
+	public void TesterLogin(String username, String pass) {
+	driver.get("https://qa.archoral.com");
+//	login.enterUsername("chirag1kirad@gmail.com");
+	login.enterUsername(username);
+//	login.enterPassword("Chirag@123123");
+	login.enterPassword(pass);
+	login.clickOnLogin();
+	}
 
 }
--- TemplatePr2h/src/test/java/testCases/TestCases.java
+++ TemplatePr2h/src/test/java/testCases/TestCases.java
@@ -8,19 +8,22 @@ import org.testng.annotations.Test;
 import pageLayer.LoginPage;
 import pageLayer.UIElement;
 import utility.TakeScreenshot;
+import utility.TesterLogin;
 //import UIElement uielement;
 
 
 public class TestCases extends testBase.TestBase{
 	public LoginPage login;
+//	public TesterLogin login;
 	
 	
 	@Test 	(priority = 1)	// 07 Aug 2023 | Tester 1    // Sample test case
 	public void loginTest() throws InterruptedException {
 		driver.get("https://qa.archoral.com");
-		login.enterUsername("chirag1kirad@gmail.com");
-		login.enterPassword("Chirag@123123");
-		login.clickOnLogin();
+		login.TesterLogin("chirag1kirad@gmail.com", "Chirag@123123");
+//		login.enterUsername("chirag1kirad@gmail.com");
+//		login.enterPassword("Chirag@123123");
+//		login.clickOnLogin();
 		TakeScreenshot.takeSS("Passed_passwordLogin");
 		}
 	
